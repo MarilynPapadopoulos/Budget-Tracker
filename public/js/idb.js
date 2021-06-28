bceda5c1a189d664538829b60d351f66c876d44f
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-06-28 19:15:00 -0400

    debug offline functionality

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,85 +1,89 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
-const request = indexedDB.open('budget_tracker', 1);
+// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
+const request = indexedDB.open("budget_tracker", 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
-request.onupgradeneeded = function(event) {
-    // save a reference to the database 
-    const db = event.target.result;
-    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('new_budget', { autoIncrement: true });
-  };
-  // upon a successful 
-request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-    db = event.target.result;
-  
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
-    if (navigator.onLine) {
-      
-       uploadBudget();
-    }
-  };
-  
-  request.onerror = function(event) {
-    // log error here
-    console.log(event.target.errorCode);
-  };
-  // This function will be executed if we attempt to submit a new pizza and there's no internet connection
-  function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['budget_tracker'], 'readwrite');
-  
-    // access the object store for `new_pizza`
-    const budgetObjectStore = transaction.objectStore('budget_tracker');
-  
-    // add record to your store with add method
-    budgetObjectStore.add(record);
-  }
+request.onupgradeneeded = function (event) {
+  // save a reference to the database
+  const db = event.target.result;
+  // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("new_budget", { autoIncrement: true });
+};
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db = event.target.result;
 
-  function uploadBudget() {
-    // open a transaction on your db
-    const transaction = db.transaction(['new_budget'], 'readwrite');
-  
-    // access your object store
-    const budgetObjectStore = transaction.objectStore('new_budget');
-  
-    // get all records from store and set to a variable
-    const getAll = budgetObjectStore.getAll();
-    
-    // upon a successful .getAll() execution, run this function
-getAll.onsuccess = function() {
+  // check if app is online, if yes run uploadBudget() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
+// This function will be executed if we attempt to submit a new budget and there's no internet connection
+function saveRecord(record) {
+  console.log(db);
+  //console.log(new_budget);
+
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["pending"], "readwrite");
+  console.log(db.transaction);
+  console.log(new_bidget);
+
+  // access the object store for `new_budget`
+  const budgetObjectStore = transaction.objectStore("pending");
+
+  // add record to your store with add method
+  budgetObjectStore.add(record);
+
+}
+
+function uploadBudget() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_budget"], "readwrite");
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("new_budget");
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/transactions/bulk', {
-        method: 'POST',
+      fetch("/api/transactions/bulk", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json'
-        }
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
       })
-        .then(response => response.json())
-        .then(serverResponse => {
+        .then((response) => response.json())
+        .then((serverResponse) => {
           if (serverResponse.message) {
             throw new Error(serverResponse);
           }
           // open one more transaction
-          const transaction = db.transaction(['new_budget'], 'readwrite');
-          // access the new_pizza object store
-          const pizzaObjectStore = transaction.objectStore('new_budget');
+          const transaction = db.transaction(["new_budget"], "readwrite");
+          // access the new_budget object store
+          const budgetObjectStore = transaction.objectStore("new_budget");
           // clear all items in your store
-          pizzaObjectStore.clear();
+          budgetObjectStore.clear();
 
-          alert('All saved budgets has been submitted!');
+          alert("All saved budgets has been submitted!");
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
         });
     }
   };
-  }
-  // listen for app coming back online
-window.addEventListener('online', uploadBudget);
-  
+}
+// listen for app coming back online
+window.addEventListener("online", uploadBudget);
